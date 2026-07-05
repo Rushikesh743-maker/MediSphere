529e782b5f0799859191de0a4979be59c85cc226
Narke Rushikesh Ashruba <hrushikeshnarake@gmail.com>
2026-07-05 12:06:15 +0530

    Add ExpenseCard component for displaying expenses

---
+++ React Projects/expense-tracker/src/components/ExpenseCard.jsx
@@ -0,0 +1,45 @@
+function ExpenseCard({ expense, onDelete, onEdit }) {
+  return (
+    <div className="expense-card">
+
+      <h3>{expense.title}</h3>
+
+      <p>
+        <strong>Amount :</strong> ₹ {expense.amount}
+      </p>
+
+      <p>
+        <strong>Type :</strong> {expense.type}
+      </p>
+
+      <p>
+        <strong>Category :</strong> {expense.category}
+      </p>
+
+      <p>
+        <strong>Date :</strong> {expense.date}
+      </p>
+
+      <div className="buttons">
+
+        <button
+          className="edit-btn"
+          onClick={() => onEdit(expense)}
+        >
+          Edit
+        </button>
+
+        <button
+          className="delete-btn"
+          onClick={() => onDelete(expense.id)}
+        >
+          Delete
+        </button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default ExpenseCard;
